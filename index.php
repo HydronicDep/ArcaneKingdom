fb0ff159e3c02bee7edf130c6e7e62c6bdce40f7
HydronicDev <153455925+HydronicDep@users.noreply.github.com>
2024-03-08 17:55:39 -0300

    Site Base

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
 <?php 
-	phpinfo();
+	$home = "casa"
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Arcane Kingdom</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imgs/logo.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/media-query.css">
+	<title>Arcane Kingdom</title>
+</head>
+<body>
+		<a href="index.php"><img src="imgs/logo.png" alt="Logo.png" class="logo"></a>
+	<main>
+		<div class="head">
+			<div class="text"><p>Bem vindo ao Arcane Kingdom</p></div>
+			<div class="buttons">
+					<a href="#" class="register"><div class="butaor">Register</div></a>	
+					<a href="#" class="download"><div class="butaod">Download</div></a>
+			</div>
+		</div>
+		<div class="bodyone">
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima sequi in nostrum voluptates, minus recusandae impedit! Corporis dignissimos dolorum ab facere minima quisquam cumque quaerat facilis, libero numquam reprehenderit modi?</p>
+		</div>
+		<div class="bodytwo">
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam aperiam, dignissimos quo aspernatur quaerat eum laborum sed, at molestias neque aut officiis explicabo iste! Animi, nesciunt aut? Ipsa, asperiores consequatur.</p>
+		</div>
+		<div class="bodythree">
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, eos tenetur? Laboriosam ratione cum earum natus id, aut quod sed delectus odit fugiat facilis dicta incidunt provident quasi sunt iure.</p>
+		</div>
+	</main>
+	<footer>
+
+	</footer>
+</body>
+</html>
